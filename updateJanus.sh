aa34d8a1be59d495a60a7491bb0f556c091e4fa6
Thomas Piotrowski <thompiot@gmail.com>
2014-12-19 16:37:58 +0100

    updateJanus looking directly into janusproject

--- updateJanus.sh
+++ updateJanus.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p app/src/main/{java,resources}/fr/janusproject/janusandroid/janus
-cp -R ../janusproject-Android/io.janusproject.kernel/src/main/java/io/janusproject/* app/src/main/java/fr/janusproject/janusandroid/janus
-cp -R ../janusproject-Android/io.janusproject.kernel/src/main/resources/io/janusproject/* app/src/main/resources/fr/janusproject/janusandroid/janus
+cp -R ../janusproject/io.janusproject.kernel/src/main/java/io/janusproject/* app/src/main/java/fr/janusproject/janusandroid/janus
+cp -R ../janusproject/io.janusproject.kernel/src/main/resources/io/janusproject/* app/src/main/resources/fr/janusproject/janusandroid/janus
 cd app/src
 find . -type f -print0 | xargs -0 sed -i '' 's/io\.janusproject/fr\.janusproject\.janusandroid\.janus/g'
